95c48c225eac073ca50f1382c0b4b27a8f6cb847
Chance Smith <chancesmith97@hotmail.com>
2020-03-11 23:35:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,9 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find{|s| s.name == item_name}
-      item
+      item = @@items.detect{|s| s.name == item_name}
+      if item 
+        
     else 
       resp.write "Route not found"
       resp.status = 404
